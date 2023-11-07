9e790279516d79c308eeccb6389371d696a8bc5e
Arthur Ferraz <arthur@quant1.com.br>
2023-11-07 07:45:36 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# wagi-tests
+
+## Configuration
+
+### Install wagi
+
+Download the latest release from [deislabs/wagi](https://github.com/deislabs/wagi/releases) and follow the instructions.
+
+## Run
+
+To serve the server to local host, do
+
+```bash
+wagi -c modules.toml
+```
+
+## Modules
+
+Both applications are written in C and compiled to wasm in order to be served via wagi.
+
+### append
+
+Any message passed as body of the request is going to be appended (creates the file if doesnt exist) to the file "data.txt".
+
+```bash
+curl http://localhost:3000/append -d "[Your message]"
+```
+
+### Quicksort
+
+This is a load generator algorithm that implements the wost case search for quicksort O($n^2$).
+
+To see all possible arguments, type:
+
+```bash
+$ curl http://localhost:3000/quicksort?--help
+Argumentos válidos:
+        --tamanho <inteiro> -> Quantidade de elementos a serem reordenados
+        --imprime -> Ativa impressão dos elementos antes e depois
+        --help -> Imprime ajuda
+```
+
+#### The space character '\\&'
+
+The command equivalent to
+
+```bash
+./executable --tamanho 20 --imprime
+```
+
+Needs to substitute ' '(space) with '\\&'
+
+```bash
+curl http://localhost:3000/quicksort?--tamanho\&20\&--imprime
+```
---
+++ append/append.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+#define MAXLEN 80
+#define EXTRA 5
+/* add 5 to account for prefix "data="*/
+#define MAXINPUT MAXLEN  + EXTRA + 2
+/* 1 for added line break, 1 for trailing NUL */
+#define DATAFILE "/data.txt"
+
+void unencode(char *src, char *last, char *dest) {
+    // converte strings formato URL para formato normal
+    for (; src != last; src++, dest++) {
+        if (*src == '+')
+            *dest = ' ';
+        else if (*src == '%') {
+            int code;
+            if (sscanf(src + 1, "%2x", &code) != 1)
+                code = '?';
+            *dest = code;
+            src += 2;
+        } else
+            *dest = *src;
+    }
+    *dest = '\n';
+    *++dest = '\0';
+}
+
+int main(void) {
+    char *lenstr;
+    char input[MAXINPUT], data[MAXINPUT];
+    long len;
+    printf("%s%c%c\n", "Content-Type:text/html;charset=iso-8859-1", 13, 10);
+    lenstr = getenv("CONTENT_LENGTH");
+    if (lenstr == NULL || sscanf(lenstr, "%ld", &len) != 1 || len > MAXLEN)
+        printf("Erro de invocação, Forma incorreta.\n");
+    else {
+        FILE *f;
+        fgets(input, len + 1, stdin);
+        unencode(input, input + len, data);
+        f = fopen(DATAFILE, "a");
+        if (f == NULL)
+            printf("Desculpe, não podemos armazenar seus dados.\n");
+        else
+        {
+            //if data starts with "data=", ignore it;
+            if (strncmp(data, "data=", EXTRA) != 0)
+                fputs(data, f);
+            else
+                fputs(data+EXTRA, f);
+            fclose(f);
+            printf("Obrigado! sua contribuição foi armazenada!\n");
+        }
+    }
+    return 0;
+}
