8ba7b2fb525868bb53026e866a672ddcd244e196
Arthur Ferraz <arthur@quant1.com.br>
2023-11-28 11:11:12 -0300

    [feat] Added docker-compose usage

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -z "$LOCAL_IP" ]; then
-    LOCAL_IP="127.0.0.1"
+if [ -z "$LOCAL_WAGI_BIND_IP" ]; then
+    LOCAL_WAGI_BIND_IP="127.0.0.1"
 fi
 
-if [ -z "$LOCAL_PORT" ]; then
-    LOCAL_PORT="3000"
+if [ -z "$LOCAL_WAGI_BIND_PORT" ]; then
+    LOCAL_WAGI_BIND_PORT="3000"
 fi
 
-wagi -c "modules.toml" -l "$LOCAL_IP"":""$LOCAL_PORT"
+wagi -c "modules.toml" -l "$LOCAL_WAGI_BIND_IP"":""$LOCAL_WAGI_BIND_PORT"
